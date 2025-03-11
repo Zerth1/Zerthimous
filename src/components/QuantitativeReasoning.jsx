72c132981658b0fdddcbad34da4b5e8fa8eeda40
Zerth1 <ryanbillah@gmail.com>
2025-03-10 22:29:34 -0400

    Test 15

--- src/components/QuantitativeReasoning.jsx
+++ src/components/QuantitativeReasoning.jsx
@@ -8,8 +8,8 @@ const exercises = [
 ]
 export default function QuantitativeReasoning() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 transform translate-y-[10vh]">
+    <div className="flex flex-col items-center justify-center dvh">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         <div className="p-25 flex items-center justify-center text-center text-3xl font-bold neon-effect bg-gradient-to-r from-red-400 to-orange-400 rounded-xl">
           Coming Soon...
         </div>
