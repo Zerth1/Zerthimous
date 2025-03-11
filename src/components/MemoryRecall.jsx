17fa96eaf9502252dd51ce61a7f37216d37d3156
Zerth1 <ryanbillah@gmail.com>
2025-03-10 21:27:04 -0400

    Test 15

--- src/components/MemoryRecall.jsx
+++ src/components/MemoryRecall.jsx
@@ -8,8 +8,8 @@ const exercises = [
 ]
 export default function MemoryRecall() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4]">
+    <div className="flex flex-col items-center justify-center dvh">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         <Link to="/mind-burst" className="p-25 flex items-center justify-center text-center text-3xl font-bold neon-effect bg-gradient-to-r from-blue-400 to-purple-500 rounded-xl">
           Mind Burst
         </Link>
