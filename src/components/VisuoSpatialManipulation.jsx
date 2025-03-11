7a68afc3c98447c851e1f1eadf31f8b8da53272d
Zerth1 <ryanbillah@gmail.com>
2025-03-10 22:32:28 -0400

    Test 15

--- src/components/VisuoSpatialManipulation.jsx
+++ src/components/VisuoSpatialManipulation.jsx
@@ -8,8 +8,8 @@ const exercises = [
 ]
 export default function VisuoSpatialManipulation() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 transform translate-y-[10vh]">
+    <div className="flex flex-col items-center justify-center dvh">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         <div className="p-25 flex items-center justify-center text-center text-3xl font-bold neon-effect bg-gradient-to-r from-green-400 to-teal-500 rounded-xl">
           Coming Soon...
         </div>
