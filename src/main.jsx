3f59a6d7473cc218dfa3d0a58f5c724f31c7f79c
Zerth1 <ryanbillah@gmail.com>
2025-03-11 12:48:48 -0400

    Test 15

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import './App.css'
 import App from './App.jsx'
 
-screen.orientation.lock("portrait")
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
